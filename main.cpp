4fb7c969ac6945a5db0d4a123084317c08348c1f
ilmar <khoseilmar@gmail.com>
2021-06-06 14:52:52 -0300

    cli para mediana

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
+#include <iostream>
+
 #include "ppm.h"
 
-using namespace std;
+int main(int argc, char **argv) {
+    std::string image_path;
+    std::cout << "Caminho da imagem: ";
+    std::cin >> image_path;
 
-int main()
-{
-//    PPM orig("imagem_grey.ppm");
-//    PPM equalized(orig.header, orig.equal());
+    std::cout << "Aplicando filtro da mediana 3x3..." << std::endl;
 
-    std::cout << "hello world" << endl;
-//    equalized.write("equal.ppm");
+    PPM ppm(image_path);
+    ppm.median(3);
+
+    std::string median_image_path;
+    std::cout << "Salvar como: ";
+    std::cin >> median_image_path;
+    ppm.write(median_image_path);
 
     return 0;
 }
